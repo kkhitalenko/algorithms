a5c969dcace6d1c62a6af61fa95a0395094bde7c
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2023-04-11 13:56:21 +0300

    solution added

---
+++ Яндекс Контест/17. Нелюбимое дело/solution.py
@@ -0,0 +1,12 @@
+def solution(node, idx):
+    def get_node_by_index(node, index):
+        while index:
+            node = node.next_item
+            index -= 1
+        return node
+    if idx == 0:
+        return node.next_item
+    previous_node = get_node_by_index(node, idx - 1)
+    next_node = get_node_by_index(node, idx + 1)
+    previous_node.next_item = next_node
+    return node
