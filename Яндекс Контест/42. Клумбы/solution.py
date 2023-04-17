a71a2119736eb2de2268ee81fa4ba4d4e349e1f4
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2023-04-17 19:54:19 +0300

    solution added

--- Яндекс Контест/42. Клумбы/solution.py
+++ Яндекс Контест/42. Клумбы/solution.py
@@ -1,14 +1,17 @@
-def get_flowerbed_borders(array, element=0, next_element=1, start=0, end=1):
+def get_flowerbed_borders(array):
+    element = start_index = 0
+    next_element = end_index = 1
     result = array[element]
     while next_element < n:
         item = array[next_element]
         changed = False
-        if item[start] <= result[start]:
-            result[start] = item[start]
+        if item[start_index] <= result[start_index]:
+            result[start_index] = item[start_index]
             changed = True
-        elif (item[start] > result[start]) and (item[start] <= result[end]):
-            if result[end] < item[end]:
-                result[end] = item[end]
+        elif ((item[start_index] > result[start_index]) and
+              (item[start_index] <= result[end_index])):
+            if result[end_index] < item[end_index]:
+                result[end_index] = item[end_index]
             changed = True
         if not changed:
             print(*result)
