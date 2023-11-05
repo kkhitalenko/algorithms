c8221da74f6c8417a83329a7aad7c91f202a7b28
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2023-11-05 18:34:35 +0300

    solution added

---
+++ Яндекс Контест/68. Добавь узел/solution.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, left=None, right=None, value=0):
+        self.right = right
+        self.left = left
+        self.value = value
+
+
+def insert(root, key) -> Node:
+    if key < root.value:
+        if root.left is None:
+            root.left = Node(value=key)
+        else:
+            insert(root.left, key)
+    else:
+        if root.right is None:
+            root.right = Node(value=key)
+        else:
+            insert(root.right, key)
+    return root
+
+
+def test():
+    node1 = Node(None, None, 7)
+    node2 = Node(node1, None, 8)
+    node3 = Node(None, node2, 7)
+    new_head = insert(node3, 6)
+    assert new_head is node3
+    assert new_head.left.value == 6
+
+
+if __name__ == '__main__':
+    test()
